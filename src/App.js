ce8c75aa74d822bba99d293aaca88f31864f7742
kgautam07 <itskrissg@gmail.com>
2023-04-19 17:52:27 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import MovieCard from "./MovieCard";
 import "./App.css";
 import SearchIcon from "./search.svg";
-const API_URL = "http://www.omdbapi.com?apikey=b78a67e2";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=b78a67e2";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
